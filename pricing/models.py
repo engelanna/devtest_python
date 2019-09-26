80e95fe59ccb7f40245071ad24311edec85173dc
Anna Engel <anna@globalapptesting.com>
2019-09-26 21:12:46 +0200

    Use m2m_changed signal to validate Country.target_groups are root nodes

--- pricing/models.py
+++ pricing/models.py
@@ -1,16 +1,16 @@
 from django.db import models
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
 from encrypted_model_fields.fields import EncryptedCharField
 
 
 class PanelProvider(models.Model):
-  def __str__(self): return "code: %s" % self.code
-
   code = models.CharField(max_length=20)
+
+  def __repr__(self): return "%s, code: %s" % (self.id, self.code)
 
 
 class TargetGroup(models.Model):
-  def __str__(self): return "%s, external_id: %s" % (self.name, self.external_id)
-
   parent = models.ForeignKey('self', null=True, on_delete=models.SET_NULL)
   panel_provider = models.ForeignKey(PanelProvider, null=True, on_delete=models.SET_NULL)
 
@@ -18,41 +18,39 @@ class TargetGroup(models.Model):
   name = models.CharField(max_length=100)
   secret_code = EncryptedCharField(max_length=100)
 
+  def __repr__(self): return "%s, name: %s, external_id: %s" % (self.id, self.name, self.external_id)
+
 
 class Country(models.Model):
-  def __str__(self): return "country_code: %s" % self.country_code
-
   panel_provider = models.ForeignKey(PanelProvider, null=True, on_delete=models.SET_NULL)
-  target_groups = models.ManyToManyField(TargetGroup) # TODO: Only root nodes
+  target_groups = models.ManyToManyField(TargetGroup)
 
   country_code = models.CharField(max_length=20)
 
+  def __repr__(self): return "%s, country_code: %s" % (self.id, self.country_code)
+
+
+@receiver(m2m_changed, sender=Country.target_groups.through)
+def are_all_target_groups_root(sender, **kwargs):
+  for pk in kwargs["pk_set"]:
+    if kwargs["model"].objects.get(pk=pk).parent != None:
+      raise ValueError("All target groups for a country must be root")
+
 
 class LocationGroup(models.Model):
-  def __str__(self): return "name: %s" % self.name
-
   country = models.ForeignKey(Country, null=True, on_delete=models.SET_NULL)
   panel_provider = models.ForeignKey(PanelProvider, null=True, on_delete=models.SET_NULL)
 
   name = models.CharField(max_length=100)
 
+  def __repr__(self): return "%s, name: %s" % (self.id, self.name)
+
 
 class Location(models.Model):
-  def __str__(self): return "name: %s" % self.name
-
   location_groups = models.ManyToManyField(LocationGroup)
 
   external_id = models.IntegerField(default=None)
   name = models.CharField(max_length=100)
   secret_code = EncryptedCharField(max_length=100)
 
-
-
-
-"""
-3 Countries, each with different panel provider
-3 Panel Providers
-20 Locations of any type (city, region, state, etc.)
-4 Location Groups, 3 of them with different provider and 1 would belong to any of them
-4 root Target Groups and each root should start a tree which is minimium 3 levels deep (3 of them with different provider and 1 would belong to any of them)
-"""
+  def __repr__(self): return "%s, name: %s" % (self.id, self.name)
